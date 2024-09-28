f3a7ab4ebc18afd93f097fdbda76b6b87a588c9f
is44co <aboodymain1@gmail.com>
2024-09-29 01:29:44 +0300

    edit

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const del = document.querySelector('.delete');
+
 const plus = document.querySelector('.operatorPlus');
 const nums = document.querySelectorAll('.num');
 const equal = document.querySelector('.results');
 const clr = document.querySelector('.clear');
+const del=document.querySelector('.delete');
+const minus=document.querySelector('.operatorMinus');
+const multiply=document.querySelector('.operatorMultiply');
+const divide=document.querySelector('.operatorDivide');
+let operators=document.querySelectorAll('.operator')
 
 function displayResult(value) {
   document.querySelector('.displayResults').textContent = value;
@@ -17,25 +22,81 @@ nums.forEach((num1) => {
   num1.addEventListener('click', ()=> {
     currentNum += num1.textContent; 
     displayResult(currentNum);
-    handleNumberClick(num1.textContent);
+ 
   });
 });
 
-plus.addEventListener('click', () => {
- 
-    firstnumber = currentNum;  
-    currentNum = '';           
-    operator = '+';            
-  
-});
+operators.forEach((operato1)=>{
+operato1.addEventListener('click',()=>{
 
-equal.addEventListener('click', () => {
+operator=operato1.textContent
+
+firstnumber = currentNum; 
+currentNum = '';   
+})
+
+
+})
+
+// plus.addEventListener('click', () => {
+ 
+//     firstnumber = currentNum;  
+//     currentNum = '';           
+               
   
+// });
+
+equal.addEventListener('click', Equal );
+
+
+function Equal(){
+
+  
+
     secondnumber = currentNum;  
-    const result = Number(firstnumber) + Number(secondnumber); 
-    displayResult(result);    
-
+     let result=equations()
+    displayResult(result);   
+  
+console.log(result)
  firstnumber=result;
+ secondnumber=currentNum
  
 
+}
+
+clr.addEventListener('click',Reset )
+function Reset(){
+  currentNum = '';
+  firstnumber = null;
+  operator = null;
+  secondnumber = null;
+  
+  displayResult(currentNum)
+}
+del.addEventListener('click', () => {
+  
+  currentNum = currentNum.slice(0, -1);
+  displayResult(currentNum ); 
 });
+
+function equations(){
+  let result=0
+  if(operator=='-'){
+   
+   result = Number(firstnumber)- Number(secondnumber)
+  }
+  if(operator==='+'){
+    
+   result=Number(firstnumber)+ Number(secondnumber)
+  }
+  if(operator=='*'){
+    
+   result=  Number(firstnumber)* Number(secondnumber)
+  }
+  if(operator=='/'){
+   
+    result= Number(firstnumber)/ Number(secondnumber)
+  }
+  return result
+  
+   }
