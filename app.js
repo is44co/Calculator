79c483e25b2e616aa00d8c7e583467130f34a0c2
is44co <aboodymain1@gmail.com>
2024-10-14 13:33:13 +0300

    edit

--- app.js
+++ app.js
@@ -18,83 +18,72 @@ let firstnumber = null;
 let operator = null;
 let secondnumber = null;
 
+let result = null;
+
 nums.forEach((num1) => {
   num1.addEventListener('click', ()=> {
-    currentNum += num1.textContent; 
+    currentNum += num1.textContent;
     displayResult(currentNum);
- 
   });
 });
 
-
 operators.forEach((operato1) => {
   operato1.addEventListener('click', () => {
+    if (currentNum === '') return; 
     if (firstnumber === null) {
       firstnumber = currentNum;
-    } else {
+    } else if (secondnumber === null) {
       secondnumber = currentNum;
-      result = equations();      
+      result = equations();
       displayResult(result);
       firstnumber = result;
     }
     operator = operato1.textContent;
-    currentNum = ''
+    currentNum = '';  
   });
 });
 
+equal.addEventListener('click', Equal);
 
-
-equal.addEventListener('click', Equal );
-
-
-function Equal(){
-
-  
-
-    secondnumber = currentNum;  
-     let result=equations()
-    displayResult(result);   
-  
-console.log(result)
- firstnumber=result;
- secondnumber=currentNum
- 
-
+function Equal() {
+  if (currentNum === '' || firstnumber === null || operator === null) return;
+  secondnumber = currentNum;
+  result = equations();
+  displayResult(result);
+  firstnumber = result;  
+  secondnumber = null;   
+  currentNum = '';       
 }
 
-clr.addEventListener('click',Reset )
-function Reset(){
+clr.addEventListener('click', Reset);
+
+function Reset() {
   currentNum = '';
   firstnumber = null;
   operator = null;
   secondnumber = null;
-  
-  displayResult(currentNum)
+  result = null;
+  displayResult('0');
 }
+
 del.addEventListener('click', () => {
-  
-  currentNum = currentNum.slice(0, -1);
-  displayResult(currentNum ); 
+  currentNum = currentNum.slice(0, -1);  
+  displayResult(currentNum || '0');      
 });
 
-function equations(){
-  let result=0
-  if(operator=='-'){
-   
-   result = Number(firstnumber)- Number(secondnumber)
+function equations() {
+  let result = 0;
+  if (operator === '-') {
+    result = Number(firstnumber) - Number(secondnumber);
+  } else if (operator === '+') {
+    result = Number(firstnumber) + Number(secondnumber);
+  } else if (operator === 'X') {
+    result = Number(firstnumber) * Number(secondnumber);
+  } else if (operator === '/') {
+    if (Number(secondnumber) === 0) {
+      return 'Error'; 
+    }
+    result = Number(firstnumber) / Number(secondnumber);
   }
-  if(operator==='+'){
-    
-   result=Number(firstnumber)+ Number(secondnumber)
-  }
-  if(operator=='X'){
-    
-   result=  Number(firstnumber)* Number(secondnumber)
-  }
-  if(operator=='/'){
-   
-    result= Number(firstnumber)/ Number(secondnumber)
-  }
-  return result
-  
-   }
+  return result;
+}
